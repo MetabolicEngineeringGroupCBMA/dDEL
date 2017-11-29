13ea195c603e9a98c3e7dad2f517c57f6acd3e6f
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2017-11-29 12:20:17 +0000

    added travis ci

---
+++ ci.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda_latest.sh
+bash Miniconda_latest.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+rm Miniconda_latest.sh
+conda config --set always_yes yes --set show_channel_urls yes
+conda update conda
+conda config --add channels BjornFJohansson
+conda create -qy -n testenv python=3.6 nbval pytest lxml requests
+source activate testenv
+which python
+python --version
+conda install pydna
+python run_test.py
---
+++ run_all.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import subprocess
+import os
+import pathlib
+
+thisdir = os.path.dirname(os.path.realpath(__file__))
+
+for dirpath, dirnames, filenames in os.walk(thisdir):
+    if dirpath.startswith(('.','_')): continue
+    for file_ in filenames:
+        if not file_.endswith(".ipynb"): continue
+        p = pathlib.Path(os.path.join(dirpath, file_))
+        if any( [f for f in p.parts if f.startswith(("_","."))] ): continue    
+        os.chdir(dirpath)
+        #cmd = 'jupyter nbconvert --execute --inplace "{}"'.format(file_)
+        cmd = ["jupyter","nbconvert","--execute","--inplace",file_]
+        print("Executing: ", " ".join(cmd))
+        #subprocess.run(cmd, shell=True)
+        subprocess.run(cmd)
+        print("----")
+print("run_all.py finished.")
+
+
+
+
